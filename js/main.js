332ac8969108e0c472bc09e545ee3f155b1e5d21
Aron Nochenson <anochenson@gmail.com>
2022-12-27 11:48:40 +0200

    add touch support

--- js/main.js
+++ js/main.js
@@ -135,8 +135,11 @@ const attachListeners = () => {
   canvas.addEventListener("mousedown", handleGrabCircle);
   canvas.addEventListener("mouseup", handleReleaseGrabbedCircle);
   canvas.addEventListener("mousemove", handleMoveGrabbedCircle);
+
   canvas.addEventListener('touchstart', (e) => execOnFingers(handleGrabCircle, e));
   canvas.addEventListener('touchend', (e) => execOnFingers(handleReleaseGrabbedCircle, e));
+  canvas.addEventListener('touchcancel', (e) => execOnFingers(handleReleaseGrabbedCircle, e));
+
   canvas.addEventListener('touchmove', (e) => execOnFingers(handleMoveGrabbedCircle, e));
 };
 
